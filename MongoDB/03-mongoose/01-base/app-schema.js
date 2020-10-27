309c72ae7c6a7e1d035a0166848714ba06d5cadb
yushihu <1832237525@qq.com>
2020-10-27 16:45:34 +0800

    update js

--- MongoDB/03-mongoose/01-base/app-schema.js
+++ MongoDB/03-mongoose/01-base/app-schema.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const moment = require('moment')
 mongoose.connect("mongodb://localhost/blog",{useNewUrlParser:true,useUnifiedTopology:true})
 const db = mongoose.connection
 
@@ -12,9 +12,28 @@ db.on("error",()=>{
 db.once('open',async ()=>{
     console.log("DB create")
     try{
-        const result = await User.insertMany({})
+        /*
+        const result = await User.insertMany({
+            name:'tom',
+            age:20,
+            score:130,
+            Date:Date.now,
+            friends:['lilei','hanmeimei'],
+            major:'history'
+        })
+        */
+       //方法一 使用date对象的tolocalString方法
+       /*
+        const result = await User.findById('5f97dbec29e7541a786277c6')
+        const date = new Date(result.createdAt)
+        console.log(date.toLocaleString())
+        */
 
-        console.log(result)
+        //方法二引用第三方库 
+        const result = await User.findById('5f97dbec29e7541a786277c6')
+        console.log(moment(result.createdAt).format('YYYY-MM-DD HH:mm:ss'))
+
+        // console.log(result)
     }catch(e){
         console.log(e)
     }
--- MongoDB/03-mongoose/01-base/models/user.js
+++ MongoDB/03-mongoose/01-base/models/user.js
@@ -2,8 +2,36 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     name:String,
-    age:Number,
-    score:Number
+    age:{
+        type:Number,
+        max:[100,'年龄最大为一百']
+    },
+    adress:{
+        type:String,
+        default:'SH'
+    },
+    score:{
+        type:Number,
+        max:[150,'成绩最大为150'],
+        min:[10,'成绩最小为10'],
+        required:[true,"必须添加成绩"]
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    isLock:{
+        type:Boolean,
+        default:false
+    },
+    friends:{
+        type:Array,
+        default:[]
+    },
+    major:{
+        type:Array,
+        default:'computer'
+    }
 })
 
 const User = mongoose.model('user',userSchema)
