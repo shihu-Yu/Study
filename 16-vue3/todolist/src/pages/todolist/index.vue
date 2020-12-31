b5666528e57931ec369673483fe77cb473b126e3
Yushihu <1832237525@qq.com>
2020-12-31 11:41:42 +0800

    up wx

---
+++ 16-vue3/todolist/src/pages/todolist/index.vue
@@ -0,0 +1,79 @@
+<template>
+     <div class="todolist">
+        <div class="head">
+            <input type="text" v-model='task' />
+            <button @click="addItem">提交</button>
+        </div>
+        <transition-group  class="list" tag='ul' name="fade" appear>
+            <li 
+                class="item"
+                v-for="item in list"
+                :key="item.id"
+                @click="delItem(item.id)"
+            >
+                {{item.task}}
+            </li>
+        </transition-group>
+        <p class="footer">共计{{total}}个项目</p>
+    </div>
+</template>
+
+<script>
+import {reactive} from 'vue'
+export default{
+    name:"Todolist", 
+    setup(){
+
+    }
+}
+</script>
+
+<style scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
+li{
+    list-style: none;
+}
+.todolist{
+    width: 800px;
+    margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.todolist .head{
+    display: flex;
+    height: 40px;
+    margin-bottom: 10px;
+}
+.todolist .head input{
+    width: 700px;
+    outline: none;
+    padding: 10px;
+    font-size: 28px;
+}
+.todolist .head button{
+    width: 100px;
+}
+.todolist .item{
+    width: 100%;
+    line-height: 40px;
+    padding: 5px 0;
+    border-bottom: 1px dashed #ccc;
+    cursor: pointer;
+}
+.todolist .item:hover{
+    background-color:#eee
+}
+.todolist .footer{
+    margin-top: 15px;
+}
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to{
+    opacity: 0;
+}
+</style>
+
---
+++ 17-wx/pages/index/index.js
@@ -0,0 +1,97 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      message:"大鱼的翅膀",
+      list:[
+        {id:1,task:"xixxi"},
+        {id:2,task:"你是我的眼"}
+      ],
+      show:true
+  },
+  // bindtap:function(){
+  //   console.log('bindtab...')
+  // },
+  bindtap:function(event){
+    // 通过event获取传递过来的数据
+    console.log(event.currentTarget.dataset.id)
+    console.log(event.currentTarget.dataset.age)
+  },
+  handleTap1:function(){
+    console.log('handleTap1...')
+  },  
+  handleTap2:function(){
+    console.log('handleTap2...')
+  },
+  handleTap3:function(){
+    console.log('handleTap3...')
+  },
+
+  //更新数据 setState 
+  updata:function(){
+    // 这里的this指向page
+    this.setData({
+      message:'明天,你好'
+    },()=>{
+      // 该回调函数是在页面渲染完成之后执行的
+      console.log('updata done')
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
